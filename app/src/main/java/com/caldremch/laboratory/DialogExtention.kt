64437c992f8825a3d89c3e4cd4698fd3a67018a2
Caldremch <caldremch@163.com>
2020-06-18 00:52:03 +0800

    添加dsl kotlin弹窗

---
+++ app/src/main/java/com/caldremch/laboratory/DialogExtention.kt
@@ -0,0 +1,14 @@
+package com.caldremch.laboratory
+
+import androidx.appcompat.app.AppCompatActivity
+import com.caldremch.dialog.TipDialog
+
+/**
+ *
+ *Created by Caldremch on 2020/6/18.
+ *
+ **/
+class DialogExtention {
+
+
+}
