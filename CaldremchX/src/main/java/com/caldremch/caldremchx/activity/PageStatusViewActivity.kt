53f5542a901337a5ef4e1f1faa41971354dcac7e
Caldremch <caldremch@163.com>
2020-06-30 13:57:23 +0800

    add caldremchX

---
+++ CaldremchX/src/main/java/com/caldremch/caldremchx/activity/PageStatusViewActivity.kt
@@ -0,0 +1,17 @@
+package com.caldremch.caldremchx.activity
+
+import com.caldremch.caldremchx.R
+import com.caldremch.common.base.AbsActivity
+
+class PageStatusViewActivity : AbsActivity() {
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_main2
+    }
+
+    override fun getTitleViewId(): Int {
+        return R.layout.test_title
+    }
+
+
+}
