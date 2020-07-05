1247fefd69de19cd57c26910f4130d3e17f1dd70
Caldremch <caldremch@CaldremchMBP.local>
2020-07-05 10:23:00 +0800

    add url

---
+++ app/src/main/java/com/caldremch/laboratory/fragment/ActionSheetFragment.kt
@@ -0,0 +1,23 @@
+package com.caldremch.laboratory.fragment
+
+import com.caldremch.laboratory.R
+import com.caldremch.laboratory.base.LaboratoryFragment
+
+/**
+ *
+ * @author Caldremch
+ *
+ * @date 2020-07-04 16:39
+ *
+ * @email caldremch@163.com
+ *
+ * @describe
+ *
+ **/
+class ActionSheetFragment : LaboratoryFragment() {
+
+    override fun getLayoutId(): Int {
+        return R.layout.fragment_action_sheet
+    }
+
+}
