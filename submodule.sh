21d0cdb763882b701d8e073a6dd75946e8b07d46
Caldremch <caldremch@163.com>
2021-11-04 18:35:12 +0800

    rm cache picker-view

--- submodule.sh
+++ submodule.sh
@@ -1,4 +1,6 @@
 #git rm -r --cached common
 #git  submodule add git@github.com:android-module/common.git
 #git  submodule add git@github.com:android-module/dialog.git
-git rm --cache -r data-core
+git rm --cache -r picker-view
+git add .
+git commit -m"rm cache picker-view"
