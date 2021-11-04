37691f1796b058027df936b6dfb3547ba3fb0484
Caldremch <caldremch@163.com>
2021-11-04 15:40:58 +0800

    add submodule

--- submodule.sh
+++ submodule.sh
@@ -1,3 +1,3 @@
 #git rm -r --cached common
 #git  submodule add git@github.com:android-module/common.git
-#git  submodule add git@github.com:android-module/dialog.git
+git  submodule add git@github.com:android-module/dialog.git
