38393b63964ecb946aa5e7ef8cd8f3e84d609199
Caldremch <caldremch@163.com>
2021-11-04 15:21:18 +0800

    update

---
+++ common/src/main/java/com/caldremch/common/jetpack/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.caldremch.common.jetpack
+
+import androidx.lifecycle.ViewModel
+
+/**
+ *
+ * @auther Caldremch
+ *
+ * @email finishmo@qq.com
+ *
+ * @date 2021/1/24 18:16
+ *
+ * @description
+ *
+ *
+ */
+open class BaseViewModel : ViewModel()
