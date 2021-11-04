0f2f2a906ded1e95447fff95be8dac5bc942dd0a
Caldremch <caldremch@163.com>
2021-11-04 21:58:24 +0800

    rm cache all

--- submodule.sh
+++ submodule.sh
@@ -1,6 +1,13 @@
 #git rm -r --cached common
 #git  submodule add git@github.com:android-module/common.git
 #git  submodule add git@github.com:android-module/dialog.git
+git rm --cache -r common
+git rm --cache -r dialog
+git rm --cache -r data-core
+git rm --cache -r image-core
 git rm --cache -r picker-view
+git rm --cache -r utils
+git rm --cache -r version-plugin
+git rm --cache -r widget
 git add .
-git commit -m"rm cache picker-view"
+git commit -m"rm cache all"
